165fba7cf9e812df89c76a86907afcea74e8441c
sh1nome <shinodayo1024@gmail.com>
2025-06-15 16:39:57 +0900

    .gitconfig.localを追加し、setup_dotfiles.shにシンボリックリンク作成を追加

--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -4,5 +4,6 @@ DOTFILES_DIR="$(cd "$(dirname "$0")"; pwd)"
 
 ln -sfn "$DOTFILES_DIR/.vimrc" "$HOME/.vimrc"
 ln -sfn "$DOTFILES_DIR/.gitconfig" "$HOME/.gitconfig"
+ln -sfn "$DOTFILES_DIR/.gitconfig.local" "$HOME/.gitconfig.local"
 
 echo "シンボリックリンクを作成しました。"
