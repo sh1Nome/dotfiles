8fd09fc91de13b3da0cb46b67b61471a15ffdc64
sh1nome <shinodayo1024@gmail.com>
2025-08-02 09:32:34 +0900

    [add] LinuxおよびWindows用のdotfiles管理スクリプトを追加
    新しいスクリプトを追加し、dotfilesのセットアップ、更新、削除を行えるようにしました。
    これにより、ユーザーは簡単に設定ファイルを管理できるようになります。

--- setup_windows/ps1/update_dotfiles.ps1
+++ setup_windows/ps1/update_dotfiles.ps1
@@ -6,11 +6,11 @@ $DOTFILES_DIR = Split-Path -Parent (Split-Path -Parent $PSScriptRoot)
 
 # まず、既存のシンボリックリンクを削除することをユーザーに伝えます。
 Write-Host "既存のシンボリックリンクを削除します..."
-& "$DOTFILES_DIR\windows\ps1\remove_dotfile_links.ps1"
+& "$DOTFILES_DIR\setup_windows\ps1\remove_dotfile_links.ps1"
 
 # 次に、新しいシンボリックリンクを作成することをユーザーに伝えます。
 Write-Host "新しいシンボリックリンクを作成します..."
-& "$DOTFILES_DIR\windows\ps1\setup_dotfiles.ps1"
+& "$DOTFILES_DIR\setup_windows\ps1\setup_dotfiles.ps1"
 
 # 全ての処理が終わったことをユーザーに伝えます。
 Write-Host "dotfilesのアップデートが完了しました。"
