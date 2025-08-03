0885ba62e223ebad0f06326aa1db0580d28a508b
sh1nome <shinodayo1024@gmail.com>
2025-08-03 20:58:12 +0900

    [add] Goのビルド用Dockerfileとスクリプトを追加
    新たにDockerfileとビルドスクリプトを追加し、GoのソースコードをDockerコンテナでビルドできるようにしました。
    これにより、バイナリを簡単に生成し、管理できるようになります。
    不要なDocker Compose関連のスクリプトは削除しました。

---
+++ setup/build.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+#############################################################################
+# このスクリプトは、GoのソースコードをDockerコンテナでビルドし、
+# 生成されたバイナリをbinディレクトリに取得するためのものです。
+# 主な処理:
+# 1. Dockerイメージのビルド
+# 2. コンテナの起動とバイナリ取得
+# 3. コンテナとイメージのクリーンアップ
+#############################################################################
+
+
+# エラーが起きたらその時点で終了
+set -e
+
+# イメージ名とコンテナ名
+IMAGE_NAME=dotfiles-golang-setup
+CONTAINER_NAME=dotfiles-golang-setup-container
+
+# setup/binディレクトリ作成
+mkdir -p ./bin
+
+# Dockerイメージをビルド
+docker build -t "$IMAGE_NAME" .
+
+# コンテナを起動（バックグラウンドで）
+docker run -d --init --name "$CONTAINER_NAME" "$IMAGE_NAME"
+
+# コンテナからbinディレクトリをコピー
+docker cp "$CONTAINER_NAME":/workspace/bin .
+
+# コンテナを停止して削除
+docker stop "$CONTAINER_NAME"
+docker rm "$CONTAINER_NAME"
+
+# イメージを削除
+docker rmi "$IMAGE_NAME"
--- setup/docker_compose_up.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-# このスクリプトはDocker Composeを起動します
-MYUID=$(id -u) MYGID=$(id -g) docker compose up -d --build
