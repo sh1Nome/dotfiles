7168ea43dd083c5b2549d24c14d46c1828e038a5
sh1nome <shinodayo1024@gmail.com>
2025-11-29 18:53:15 +0900

    [update]srcディレクトリを削除、build.shを修正
    srcディレクトリを削除し、build.shがdockerに依存しないように修正しました。
    かわりに、ホストOSにgo環境がある必要があります。

--- setup/build.sh
+++ setup/build.sh
@@ -16,29 +16,17 @@ set -e
 # スクリプトのあるディレクトリ（setup/）に移動
 cd "$(dirname "$0")"
 
-# イメージ名とコンテナ名
-IMAGE_NAME=dotfiles-golang-setup
-CONTAINER_NAME=dotfiles-golang-setup-container
-
 # setup/binディレクトリのクリーン
 rm -rf ./bin
 mkdir -p ./bin
 
-# Dockerイメージをビルド
-docker build --no-cache -t "$IMAGE_NAME" .
-
-# コンテナを起動（バックグラウンドで）
-docker run -d --init --name "$CONTAINER_NAME" "$IMAGE_NAME"
-
-# コンテナからbinディレクトリをコピー
-docker cp "$CONTAINER_NAME":/workspace/bin .
-
-# コンテナを停止して削除
-docker stop "$CONTAINER_NAME"
-docker rm "$CONTAINER_NAME"
-
-# イメージを削除
-docker rmi "$IMAGE_NAME"
+# ビルド
+echo "Building $f for linux/amd64..."
+GOOS=linux GOARCH=amd64 go build -v -o "bin/setup-linux-amd64" "setup.go"
+GOOS=linux GOARCH=amd64 go build -v -o "bin/remove-linux-amd64" "remove.go"
+echo "Building $f for windows/amd64..."
+GOOS=windows GOARCH=amd64 go build -v -o "bin/setup-windows-amd64.exe" "setup.go"
+GOOS=windows GOARCH=amd64 go build -v -o "bin/remove-windows-amd64.exe" "remove.go"
 
 # bin以下のファイルに実行権限を付与
 chmod +x ./bin/*
