a1e1bbb6bdc2ad54f4ef29129de53d03ab32e32e
sh1nome <shinodayo1024@gmail.com>
2026-03-22 22:56:24 +0900

    [update] wezterm のタブバーの設定をシンプルに

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -12,43 +12,8 @@ end
 
 -- タブ
 config.show_new_tab_button_in_tab_bar = false -- タブバーの「+」ボタンを非表示
-config.tab_bar_at_bottom = true -- タブバーをウィンドウの下に表示
-config.use_fancy_tab_bar = false -- レトロなタブバーを使用する
+config.show_close_tab_button_in_tabs = false -- タブバーの「x」ボタンを非表示
 config.tab_max_width = 32 -- タブの最大幅
-
-local inactive_tab = {
-	bg_color = "#5c6d74", -- 非アクティブなタブの背景色
-	fg_color = "#ffffff", -- 非アクティブなタブの文字色
-}
-config.colors = {
-	tab_bar = {
-		background = "none", -- タブバーの背景透過
-
-		-- アクティブタブ
-		active_tab = {
-			-- タブの背景色
-			bg_color = "#ae8b2d",
-			-- タブの文字色
-			fg_color = "#ffffff",
-		},
-
-		-- 非アクティブなタブ
-		inactive_tab = inactive_tab,
-		inactive_tab_hover = inactive_tab,
-	},
-}
-
--- タブバーの表示カスタマイズ
-wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
-	local title = tab.active_pane.title
-	-- パディング分を確保してテキストを切り詰め
-	if wezterm.column_width(title) > max_width - 4 then
-		title = wezterm.truncate_right(title, max_width - (4 + wezterm.column_width("…"))) .. "…"
-	end
-	return {
-		{ Text = "  " .. title .. "  " },
-	}
-end)
 
 -- フォント
 config.font = wezterm.font("Noto Sans Mono CJK JP") -- フォントを指定
