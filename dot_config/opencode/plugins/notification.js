b3ffa8cb6922e18ad78f1816fdeab66fb9a1943a
sh1nome <shinodayo1024@gmail.com>
2026-06-18 14:01:10 +0900

    remove: codex, opencode, copilot

--- dot_config/opencode/plugins/notification.js
+++
@@ -1,9 +0,0 @@
-export const NotificationPlugin = async ({ $ }) => {
-  return {
-    event: async ({ event }) => {
-      if (event.type === "permission.asked" || event.type === "session.idle") {
-        await $`sh -c 'notify OpenCode ${event.type}'`;
-      }
-    },
-  };
-};
